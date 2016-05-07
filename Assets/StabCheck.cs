0a5bb789890a76c10425ef6d79466740e656c08b
AnagramMC <hisham.ata@hotmail.com>
2016-05-07 18:43:27 -0400

    stabby stab works

---
+++ Assets/StabCheck.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StabCheck : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter (Collider target)
+    {
+        if (target.gameObject.tag == "RangedEnemy")
+        {
+            Destroy(target.gameObject.transform.parent);
+        }
+    }
+}
